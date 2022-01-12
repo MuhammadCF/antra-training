82aa753399401209b8c0427e62a67c7f3b0357cc
Muhammad Fajar <ochafajar@gmail.com>
2022-01-12 10:53:53 -0800

    initial copying

---
+++ Week2/ioc/MyContext.java
@@ -0,0 +1,14 @@
+package com.example.java23il2021.week4.ioc;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MyContext {
+    public static void main(String[] args){
+        System.out.println();
+    }
+}
